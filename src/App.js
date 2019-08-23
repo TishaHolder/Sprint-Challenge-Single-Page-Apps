0380800d4db73eab7056b6bc80964d23ff1298c9
Tisha Holder <50963507+TishaHolder@users.noreply.github.com>
2019-08-23 12:34:28 -0400

    completed display components and added routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React from "react";
+import {Route} from "react-router-dom";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
+import WelcomePage from "./components/WelcomePage.js";
 import CharacterList from "./components/CharacterList.js";
 import LocationsList from "./components/LocationsList.js"; 
-
+import EpisodeList from "./components/EpisodeList.js";
 
 export default function App() {
   return (
     <main>
       <Header />
-      {/* <TabNav /> */}
+      <TabNav /> 
 
       {/*<CharacterList />*/}
       {/*<LocationsList />*/}
 
-   
+      {/*<EpisodeList />*/}
+
+      <Route  exact path = "/" component = {WelcomePage} /> 
+  
+      <Route  path = "/characters" component = {CharacterList} /> 
+
+      <Route  path = "/locations" component = {LocationsList} />
+
+      <Route  path = "/episodes" component = {EpisodeList} />    
 
     </main>
   );
--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -1,11 +1,42 @@
 import React from "react";
 import { Tab, Menu, Icon } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
+import 'semantic-ui-css/semantic.css'; 
+import 'semantic-ui-css/semantic.min.css'; 
+
+import {Route} from "react-router-dom";
+import WelcomePage from "./WelcomePage.js";
+import CharacterList from "./CharacterList.js";
+import LocationList from "./LocationsList.js";
+import EpisodeList from "./EpisodeList.js";
 
 // TODO: Add missing tabs below
 // Take a look at React Semantic UI tabs 
 // https://react.semantic-ui.com/modules/tab/
 export default function TabNav() {
 
-};
+    const createLabel = (iconName, labelText) => <span><Icon name={iconName} />{labelText}</span>
+
+    const welcomeLabel = createLabel("home", "Home Page")
+    const characterLabel = createLabel("users", "Characters")
+    const locationsLabel = createLabel("map", "Locations")
+    const episodesLabel = createLabel("video", "Episodes")
+
+    const panes = [
+    { menuItem: <Menu.Item key='home' as={NavLink} to={`/`} content={welcomeLabel} /> },
+    { menuItem: <Menu.Item key='characters' as={NavLink} to={`/characters`} content={characterLabel} /> },
+    { menuItem: <Menu.Item key='locations' as={NavLink} to={`/locations`} content={locationsLabel} /> },
+    { menuItem: <Menu.Item key='episodes' as={NavLink} to={`/episodes`} content={episodesLabel} /> },
+    ]
+
+
+    return (
+
+        <Tab panes={panes} renderActiveOnly={false} />
+        
+        );
+
+}
+
+
 
